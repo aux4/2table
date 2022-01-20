14ef409a35b9e7eb99afe5a5927cb3cc0ee1f60c
David Sobreira Gouvea <davidsgbr@gmail.com>
2022-01-20 00:29:39 -0800

    prepare initial version to release

--- executable.js
+++ executable.js
@@ -1,2 +1,37 @@
 #!/usr/bin/env node
 
+const { Table, Config, parseStructure, prepareData } = require(".");
+const { readStdIn } = require("./lib/Input");
+
+const args = process.argv.splice(2);
+
+const structure = args[0];
+const tableStructure = parseStructure(structure);
+
+const config = new Config(tableStructure);
+
+let input;
+
+try {
+  input = JSON.parse(readStdIn());
+} catch (e) {
+  console.error("Invalid JSON input");
+  process.exit(1);
+}
+
+let data;
+
+try {
+  data = prepareData(input, tableStructure, config);
+} catch (e) {
+  console.error("Error parsing the data");
+  process.exit(2);
+}
+
+try {
+  const table = new Table(data, tableStructure, config);
+  console.log(table.print());
+} catch (e) {
+  console.error("cannot print the table");
+  process.exit(3);
+}
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const { Config } = require("./lib/Config");
 const { Table } = require("./lib/Table");
+const { parseStructure } = require("./lib/Structure");
+const { prepareData } = require("./lib/Data");
 
-module.exports = { Table };
+module.exports = { Table, Config, parseStructure, prepareData };
