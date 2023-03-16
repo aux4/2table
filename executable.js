41b2e5247f891caca2d303a8a2cc45634be89aab
David Sobreira Gouvea <davidsgbr@gmail.com>
2023-03-16 13:17:18 -0700

    fix input reader

--- executable.js
+++ executable.js
@@ -1,37 +1,47 @@
 #!/usr/bin/env node
 
+process.stdin.setEncoding("utf8");
+
 const { Table, Config, parseStructure, prepareData } = require(".");
 const { readStdIn } = require("./lib/Input");
 
 const args = process.argv.splice(2);
 
 const structure = args[0];
+if (!structure) {
+  console.error(`Attribute columns are not specified:\n${"2table <columns comma separated>".cyan}`);
+  process.exit(1);
+}
+
 const tableStructure = parseStructure(structure);
 
 const config = new Config(tableStructure);
 
-let input;
+(async () => {
+  let input;
 
-try {
-  input = JSON.parse(readStdIn());
-} catch (e) {
-  console.error("Invalid JSON input");
-  process.exit(1);
-}
+  try {
+    const inputString = await readStdIn();
+    input = JSON.parse(inputString);
+  } catch (e) {
+    console.error("Invalid JSON input");
+    process.exit(1);
+  }
 
-let data;
+  let data;
 
-try {
-  data = prepareData(input, tableStructure, config);
-} catch (e) {
-  console.error("Error parsing the data");
-  process.exit(2);
-}
+  try {
+    data = prepareData(input, tableStructure, config);
+  } catch (e) {
+    console.error("Error parsing the data");
+    process.exit(2);
+  }
 
-try {
-  const table = new Table(data, tableStructure, config);
-  console.log(table.print());
-} catch (e) {
-  console.error("cannot print the table");
-  process.exit(3);
-}
+  try {
+    const table = new Table(data, tableStructure, config);
+    console.log(table.print());
+  } catch (e) {
+    console.error("cannot print the table");
+    process.exit(3);
+  }
+})();
